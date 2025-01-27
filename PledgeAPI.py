c92dd2c1bafba840505653260954017076631881
Kol-H <arherget@uncg.edu>
2025-01-27 16:29:35 -0500

    Added Pledge API python script and call to it (in GMapsAPI.js)

---
+++ PledgeAPI.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+import sys
+import requests
+
+lat = sys.argv[1]
+lon = sys.argv[2]
+cause_id = sys.argv[3]
+api_key = sys.argv[4]
+
+
+url = 'https://api.pledge.to/v1/organizations?per_page=100&lat=' + str(lat) + '&lon=' + str(lon) + '&cause_id=' + str(cause_id)
+
+
+headers = {
+    'Authorization' : 'Bearer ' + api_key
+}
+response = requests.get(url, headers = headers)
+
+if response.status_code == 200:
+    data = response.json()
+    print("Now showing " + str(len(data["results"])) + " results out of " + str(data["total_count"]) + "\n\n")
+    for i in range(len(data["results"])) :
+        print(str(i) + " - " + data["results"][i]["name"])
+        print("\t" + str(data["results"][i]["lat"]) + ", " + str(data["results"][i]["lon"]) + "\tregion: " + data["results"][i]["region"] + "\n")
+
+else:
+    print(response.status_code)
+    
+sys.stdout.flush()
+
+
+# In[ ]:
+
+
+#have ids for option buttons as cause_id. Figure out how to do multiple values in a param (not sure if possible)
+#only include organizations that were put into the system? Or import ALL organizations? Probably not all
+
