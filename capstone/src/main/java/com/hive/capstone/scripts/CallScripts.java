1d2910e479bcb9b3750003969fa5b7b2f45c236b
Kol-H <arherget@uncg.edu>
2025-02-14 11:21:17 -0500

    created donations page, added new PledgeAPI call

--- capstone/src/main/java/com/hive/capstone/scripts/CallScripts.java
+++ capstone/src/main/java/com/hive/capstone/scripts/CallScripts.java
@@ -6,17 +6,23 @@ import java.io.InputStreamReader;
 
 public class CallScripts {
     public static void main(String[] args) throws Exception{
-        callPy(36.098104, -79.784872, 91);
+        // 1 - search by coords
+        // 2 - search by query
+        //getOrgs(1, String.valueOf(36.098104), String.valueOf(-79.784872), String.valueOf(91)); //example coord search
+        //getOrgs(2, "Red Cross");
     }
 
-    //straight-up DOES work O.o
-    public static void callPy(double lat, double lon, int causeID) throws Exception {
+    public static void getOrgs(int searchID, String... args) throws Exception {
+        String argString = "";
+        for (int i = 0; i < args.length - 1; i++) {
+            argString += args[i] + "; ";
+        }
+        argString += args[args.length - 1];
         ProcessBuilder pyProc = new ProcessBuilder(
             "python", 
             "capstone/src/main/java/com/hive/capstone/scripts/PledgeAPI.py", 
-            String.valueOf(lat),
-            String.valueOf(lon),
-            String.valueOf(causeID)
+            String.valueOf(searchID),
+            argString
         );
 
         pyProc.redirectErrorStream(true);
@@ -33,6 +39,26 @@ public class CallScripts {
         resultReader.close();
     }
 
+    public static void getOrgByName(String name) throws Exception{
+        ProcessBuilder pyProc = new ProcessBuilder(
+            "python", 
+            "capstone/src/main/java/com/hive/capstone/scripts/PledgeAPI.py", 
+            name
+        );
+
+        pyProc.redirectErrorStream(true);
+
+        Process process = pyProc.start();
+        BufferedReader resultReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        
+        System.out.println(resultReader.readLine());
+        while (resultReader.ready()) {
+            System.out.println(resultReader.readLine());
+        }
+        int exitCode = process.waitFor();
+        System.out.println("Exited with code " + exitCode);
+        resultReader.close();
+    }
 }
 
 
--- capstone/src/main/java/com/hive/capstone/scripts/PledgeAPI.py
+++ capstone/src/main/java/com/hive/capstone/scripts/PledgeAPI.py
@@ -1,37 +1,56 @@
 import sys
 import requests
 
+def getOrgsByCoords(lat, lon, cause_id) :
+    url = 'https://api.pledge.to/v1/organizations?per_page=' + str(results_num) + '&lat=' + str(lat) + '&lon=' + str(lon) + '&cause_id=' + str(cause_id)
+
+    headers = {
+        'Authorization' : 'Bearer ' + api_key
+    }
+    response = requests.get(url, headers = headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        print("Now showing " + str(len(data["results"])) + " results out of " + str(data["total_count"]) + "\n\n")
+        for i in range(len(data["results"])) :
+            print(str(i) + " - " + data["results"][i]["name"])
+            print("\t" + str(data["results"][i]["lat"]) + ", " + str(data["results"][i]["lon"]) + "\tregion: " + data["results"][i]["region"] + "\n")
+
+    else:
+        print(response.status_code)
+        
+    sys.stdout.flush()
+
+def getOrgsByQuery(query) :
+    url = 'https://api.pledge.to/v1/organizations?per_page=' + str(results_num) + '&q=' + query
+
+    headers = {
+        'Authorization' : 'Bearer ' + api_key
+    }
+    response = requests.get(url, headers = headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        print("Now showing " + str(len(data["results"])) + " results out of " + str(data["total_count"]) + "\n\n")
+        for i in range(len(data["results"])) :
+            print(str(i) + " - " + data["results"][i]["name"])
+            print("\t" + str(data["results"][i]["lat"]) + ", " + str(data["results"][i]["lon"]) + "\tregion: " + data["results"][i]["region"] + "\n")
+
+    else:
+        print(response.status_code)
+        
+    sys.stdout.flush()
+
 results_num = 5
-lat = sys.argv[1]
-lon = sys.argv[2]
-cause_id = sys.argv[3]
 api_key = "a593e05baba4b232d7cab6df192239d0"
+search_id = sys.argv[1]
 
 
-url = 'https://api.pledge.to/v1/organizations?per_page=' + str(results_num) + '&lat=' + str(lat) + '&lon=' + str(lon) + '&cause_id=' + str(cause_id)
-
-
-headers = {
-    'Authorization' : 'Bearer ' + api_key
-}
-response = requests.get(url, headers = headers)
-
-if response.status_code == 200:
-    data = response.json()
-    print("Now showing " + str(len(data["results"])) + " results out of " + str(data["total_count"]) + "\n\n")
-    for i in range(len(data["results"])) :
-        print(str(i) + " - " + data["results"][i]["name"])
-        print("\t" + str(data["results"][i]["lat"]) + ", " + str(data["results"][i]["lon"]) + "\tregion: " + data["results"][i]["region"] + "\n")
-
-else:
-    print(response.status_code)
-    
-sys.stdout.flush()
-
-
-# In[ ]:
-
-
+if search_id == "1" : 
+    lat, lon, cause_id = sys.argv[2].split("; ")
+    getOrgsByCoords(lat = lat, lon = lon, cause_id = cause_id)
+elif search_id == "2" :
+    getOrgsByQuery(query = sys.argv[2])
 #have ids for option buttons as cause_id. Figure out how to do multiple values in a param (not sure if possible)
 #only include organizations that were put into the system? Or import ALL organizations? Probably not all
 
