7398beb41e452945f17e7962b44cffce851fa6a3
jenniferxgt <157390766+jenniferxgt@users.noreply.github.com>
2025-02-22 16:32:01 -0500

    donation service updates

--- capstone/src/main/java/com/hive/capstone/donations/DonationRepository.java
+++ capstone/src/main/java/com/hive/capstone/donations/DonationRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DonationRepository extends JpaRepository <Donation, Integer> {
@@ -14,7 +15,7 @@ public interface DonationRepository extends JpaRepository <Donation, Integer> {
     List<Donation> findByOrganizationName(String organizationName);
 
     List<Donation> findDonationName(String donationName);
-    
+
     List<Donation> findByAmount(double amount);
 
     List<Donation> findByDonateAt(Date donatedAt);
--- capstone/src/main/java/com/hive/capstone/donations/DonationService.java
+++ capstone/src/main/java/com/hive/capstone/donations/DonationService.java
@@ -1,5 +1,22 @@
 package com.hive.capstone.donations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.hive.capstone.users.User;
+import com.hive.capstone.organizations.Organization;
 
+import java.util.List;
+
+@Service
 public class DonationService {
-    
+
+    @Autowired
+    DonationRepository donationRepository;
+
+    //Get all donations
+    public List<Donation> getAllDonations(){
+        return donationRepository.findAll();
+    }
+    //Get donations by user_id
+
+
 }
