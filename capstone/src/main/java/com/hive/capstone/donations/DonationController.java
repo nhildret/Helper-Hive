f2488f397ab72171cbbf1de2b874209e9b4e2f73
Kol-H <arherget@uncg.edu>
2025-02-22 15:05:33 -0500

    added donation-page.html back in and added a mapping in DonationController

--- capstone/src/main/java/com/hive/capstone/donations/DonationController.java
+++ capstone/src/main/java/com/hive/capstone/donations/DonationController.java
@@ -1,5 +1,22 @@
 package com.hive.capstone.donations;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import com.hive.capstone.donations.*;
+
+
+@RequestMapping("/donate")
 public class DonationController {
-    
+    @GetMapping({"", "/"})
+    public String donate(Model model) {
+        // filters: local, location (state/country), cause, query
+        //DonationService.getOrgs();
+        //model.addAttribute("orgs", orgsList);
+        return "donation-page";
+    }
 }
