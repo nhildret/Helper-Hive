1278cd6944a0e6af0d07c9e817ca59c27051cdd9
Kol-H <arherget@uncg.edu>
2025-04-02 12:33:49 -0400

    created Dockerfile file and small syntax changes to make .jar file for deployment

--- capstone/src/main/java/com/hive/capstone/security/SecurityConfig.java
+++ capstone/src/main/java/com/hive/capstone/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                         .requestMatchers("/user/**").authenticated()
                         .requestMatchers("/static/**", "/images/**", "/js/**", "/css/**").permitAll()
                         .requestMatchers("/", "/login", "/about", "/users/createForm", "/users/new").permitAll()
-                        .requestMatchers("/images/**", "css/**").permitAll()// Allow access to static files
+                        .requestMatchers("/images/**", "/css/**").permitAll()// Allow access to static files
                         .requestMatchers("/admin/**").hasAuthority("Admin")
                         .requestMatchers("/OrganizationRepresentative/**").hasAuthority("OrganizationRepresentative")
 
